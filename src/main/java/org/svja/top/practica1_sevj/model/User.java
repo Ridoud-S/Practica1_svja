d11e4a92fe5606dc07950e9ff4601967463fd5d2
Jaime Serrano <ridoudyt@gmail.com>
2026-02-17 10:43:41 -0600

    feat(model): añadir modelo Usuario con datos de sesión y factory method fromSimulator

---
+++ src/main/java/org/svja/top/practica1_sevj/model/User.java
@@ -0,0 +1,54 @@
+package org.svja.top.practica1_sevj.model;
+
+import lombok.*;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User {
+    // Datos de entrada
+    private double currentWeight;
+    private double targetWeightLoss;
+    private double runningSpeed;
+    private int minutesPerSession;
+
+    // Resultados calculados
+    private double totalCalories;
+    private double caloriesPerMinute;
+    private double caloriesPerHour;
+    private double totalHours;
+
+    // Sesiones calculadas para cada duración
+    private int sessions30;
+    private int sessions45;
+    private int sessions60;
+    private int sessions75;
+    private int sessions90;
+    private int sessions120;
+
+    /**
+     * Constructor de conveniencia: crea un Usuario desde un Simulator
+     * ya calculado más los resultados de sesiones.
+     */
+    public static User fromSimulator(Simulator sim,
+                                        int s30, int s45, int s60,
+                                        int s75, int s90, int s120) {
+        User u = new User();
+        u.setCurrentWeight(sim.getCurrentWeight());
+        u.setTargetWeightLoss(sim.getTargetWeightLoss());
+        u.setRunningSpeed(sim.getRunningSpeed());
+        u.setMinutesPerSession(sim.getMinutesPerSession());
+        u.setTotalCalories(sim.getTotalCalories());
+        u.setCaloriesPerMinute(sim.getCaloriesPerMinute());
+        u.setCaloriesPerHour(sim.getCaloriesPerHour());
+        u.setTotalHours(sim.getTotalHours());
+        u.setSessions30(s30);
+        u.setSessions45(s45);
+        u.setSessions60(s60);
+        u.setSessions75(s75);
+        u.setSessions90(s90);
+        u.setSessions120(s120);
+        return u;
+    }
+}
